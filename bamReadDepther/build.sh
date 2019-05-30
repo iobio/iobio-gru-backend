6b08fd6a3fe0bc6b2283154854b0792836ea3783
Anders Pitman <tapitman11@gmail.com>
2019-05-30 14:38:18 -0600

    Add bamReadDepther recipe
    If only all builds were so simple

---
+++ bamReadDepther/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get update && apt-get -y install wget clang zlib1g-dev
+
+# samtools
+wget -q https://raw.githubusercontent.com/chmille4/bamReadDepther/master/bamReadDepther.cpp
+
+clang++ -o bamReadDepther -static -O3 bamReadDepther.cpp
+
+mv bamReadDepther build/bamReadDepther
