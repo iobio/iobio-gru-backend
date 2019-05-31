d35a14f4d5798fc561af5d34ea98c55ac637cb81
Anders Pitman <tapitman11@gmail.com>
2019-05-31 17:23:23 -0600

    Implement some error handling
    Also added pipeline creation helper functions.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,8 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const cors = require('@koa/cors');
 
+const { run } = require('./process');
 
-function run(tool, args) {
-  const toolPath = './tools/' + tool;
-  const child = spawn(toolPath, args);
-  return child;
-}
 
 class RPCServer {
   constructor(config) {
@@ -18,28 +14,99 @@ class RPCServer {
     this._router = new Router();
 
     for (const methodName in config) {
-      this._router.get('/' + methodName, (ctx, next) => {
+      this._router.get('/' + methodName, async (ctx, next) => {
 
         // TODO: error handling for invalid params
         const params = this._parseParams(config[methodName], ctx.query);
-        const pipeline = config[methodName].pipeline(params);
+        console.log(methodName);
+        console.log(config[methodName]);
+        const pipeline = config[methodName].pipeline(params).pipeline;
 
-        //const command = pipeline[0];
+        // There is an attempt at some error handling in the block below.
+        // The basic algorithm is listen for stderr (can be ignored) and
+        // exit with code !== 0 for all processes in the pipeline. Also listen
+        // for stdout of the last process. If stderr or exit !== 0 occurs
+        // before a specified amount of stdout buffer is received, then
+        // consider it an error condition.
+        // If an error occurs after we've started streaming, we're currently
+        // not handling that. I think we'd either need to add HTTP trailers
+        // or switch to WebSockets.
+        const promise = new Promise((resolve, reject) => {
 
-        let prev;
-        let child;
+          let aborted = false;
 
-        for (const stage of pipeline) {
-          child = run(stage[0], stage.slice(1));
+          function onStderr(data) {
+            if (!aborted) {
+              aborted = true;
 
-          if (prev) {
-            prev.stdout.pipe(child.stdin);
+              reject(data);
+            }
           }
 
-          prev = child;
-        }
+          function onExit(e) {
+            if (!aborted) {
+              aborted = true;
 
-        ctx.body = child.stdout;
+              // TODO: if already started streaming, either send an HTTP
+              // trailer here or switch to WebSockets so we can report the
+              // error.
+              if (e !== 0) {
+                reject("ERROR: Process exited with code: " + e);
+              }
+            }
+          }
+
+
+          let prev;
+          let child;
+
+          for (const stage of pipeline) {
+            console.log(new Date());
+
+            const name = stage[0];
+            const args = stage[1];
+            const options = stage[2];
+
+            child = spawn('./tools/' + name, args);
+
+            if (options && options.ignoreStderr !== true) {
+              child.stderr.setEncoding('utf8');
+              child.stderr.on('data', onStderr);
+            }
+
+            child.on('exit', onExit);
+
+            if (prev) {
+              prev.stdout.pipe(child.stdin);
+            }
+
+            prev = child;
+          }
+
+          const BUF_SIZE = 1024;
+          let buf = "";
+          function onStdout(data) {
+
+            buf += data;
+
+            if (buf.length > BUF_SIZE) {
+              child.stdout.removeListener('data', onStdout);
+              child.stdout.pause();
+              child.stdout.unshift(buf);
+              resolve(child.stdout);
+            }
+          }
+
+          child.stdout.on('data', onStdout);
+        });
+
+        try {
+          const stream = await promise;
+          ctx.body = stream;
+        }
+        catch (e) {
+          ctx.throw(e);
+        }
       });
     }
 
@@ -76,6 +143,26 @@ class RPCServer {
   }
 }
 
+
+function call(name, args, options) {
+  return new ToolCall(name, args, options);
+}
+
+class ToolCall {
+  constructor(name, args, options) {
+    this.pipeline = [
+      [name, args, options]
+    ];
+  }
+
+  pipe(toolCall) {
+    this.pipeline = this.pipeline.concat(toolCall.pipeline);
+    return this;
+  }
+}
+
+
 module.exports = {
   RPCServer,
+  call,
 };
