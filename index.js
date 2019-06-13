422ae0e452698721623b48e7aa7ab734cbcfa53b
Anders Pitman <tapitman11@gmail.com>
2019-06-13 16:00:48 -0600

    Add remaining bam endpoints

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const Router = require('koa-router');
 const cors = require('@koa/cors');
 const path = require('path');
 const { run } = require('./process.js');
-
 const spawn = require('child_process').spawn;
 const process = require('process');
 
@@ -11,18 +10,42 @@ const process = require('process');
 const app = new Koa();
 const router = new Router();
 
-router.get('/indexReadDepth', async (ctx) => {
+
+router.get('/alignmentHeader', async (ctx) => {
+  await handle(ctx, 'alignmentHeader.sh', [ctx.query.url]);
+});
+
+router.get('/baiReadDepth', async (ctx) => {
+  await handle(ctx, 'baiReadDepth.sh', [ctx.query.url]);
+});
+
+router.get('/craiReadDepth', async (ctx) => {
+  await handle(ctx, 'craiReadDepth.sh', [ctx.query.url]);
+});
+
+router.get('/alignmentStatsStream', async (ctx) => {
+
+  const regions = JSON.parse(ctx.query.regions);
+
+  const samtoolsRegions = regions.map(function(d) { return d.name+ ":"+ d.start + '-' + d.end;}).join(' ');
+  const bamstatsRegions = JSON.stringify(regions.map(function(d) { return {start:d.start,end:d.end,chr:d.name};}));
+
+  await handle(ctx, 'alignmentStatsStream.sh', [ctx.query.url, samtoolsRegions, bamstatsRegions]);
+});
+
+
+async function handle(ctx, scriptName, args) {
   try {
-    const scriptPath = path.join(__dirname, '/scripts/indexReadDepth.sh');
-    const proc = await run(scriptPath, [ctx.query.url]);
+    const scriptPath = path.join(__dirname, '/scripts', scriptName);
+    const proc = await run(scriptPath, args);
     ctx.body = proc.stdout;
   }
   catch (e) {
     console.error(e);
     ctx.status = 500;
+    ctx.body = e;
   }
-});
-
+}
 
 app
   .use(cors())
