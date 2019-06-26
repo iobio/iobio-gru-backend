522a565e4ffbc42947fbb0737461312cb7c089dc
Anders Pitman <tapitman11@gmail.com>
2019-06-26 17:42:02 -0600

    Add samtools_od to makefile

--- tools/download_tool.sh
+++ tools/download_tool.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 tool=${1}
 
-wget https://iobio.s3.amazonaws.com/backend_tools/${tool} -O tool_bin/${tool}
+curl https://iobio.s3.amazonaws.com/backend_tools/${tool} > tool_bin/${tool}
 chmod +x tool_bin/${tool}
