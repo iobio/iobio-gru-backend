74850af7db0fe6dd92d01199fcc3990ba936658a
Anders Pitman <tapitman11@gmail.com>
2019-07-18 17:09:28 -0600

    Use versioned backend-worker names

--- get_aws_addresses.py
+++ get_aws_addresses.py
@@ -7,5 +7,5 @@ response = client.describe_instances()
 for reservation in response['Reservations']:
     for instance in reservation['Instances']:
         for tag in instance['Tags']:
-            if instance['State']['Name'] == 'running' and tag['Key'] == 'Name' and tag['Value'] == 'backend-worker':
+            if instance['State']['Name'] == 'running' and tag['Key'] == 'Name' and tag['Value'].startswith('backend-worker'):
                 print(instance['PublicDnsName'])
