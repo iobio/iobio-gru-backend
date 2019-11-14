82e91dfbdbcdfe5457f6918fdd399c4b471e1102
Ubuntu <ubuntu@ip-172-30-0-135.ec2.internal>
2019-11-14 22:10:35 +0000

    Updated index.js to use new post paradigm; updated clinvarCounts and knownVariants; added new service getIdColumns

--- tools/knownVariants/knownVariants.py
+++ tools/knownVariants/knownVariants.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jun 12 16:51:04 2017
-
 @author: tonyd
+
+Updated 30Sept2019 by stephg
 """
 
 import vcf
@@ -11,23 +12,30 @@ import sys
 import getopt
 import math
 
-usage = 'clinvarSummary.py -r <region> -b <binLength> -p <regionParts>'
-theArgs           = {'binLength': int(0), 'regionParts': None}
-region            = {}
-CLINVAR_PATH      = ['4', '5']
-CLINVAR_BENIGN    = ['2', '3']
-CLINVAR_UNKNOWN   = ['0', '1']
-CLINVAR_OTHER     = ['6', '7', '255']
+usage = 'clinvarSummary.py -r <region> -b <binLength> -p <regionParts> -m <annotationMode>'
+theArgs = {'binLength': int(0), 'regionParts': None}
+region = {}
 
+# S3 Version (OLD Version)
+#CLINVAR_PATH = ['4', '5']
+#CLINVAR_BENIGN = ['2', '3']
+#CLINVAR_UNKNOWN = ['0', '1']
+#CLINVAR_OTHER = ['6', '7', '255']
 
+# FTP Version (NEW Version)
+CLINVAR_PATH = ['Pathogenic', 'Pathogenic/Likely_pathogenic', 'Likely_pathogenic']
+CLINVAR_BENIGN = ['Likely_benign', 'Benign', 'Benign/Likely_benign']
+CLINVAR_UNKNOWN = ['Uncertain_significance', 'not_provided']
+CLINVAR_OTHER = ['Conflicting_interpretations_of_pathogenicity', 'drug_response']
+IMPACT_FIELDS = ['LOW', 'MODIFIER', 'MODERATE', 'HIGH']
 
 def getArgs(argv):
     try:
-        opts, args = getopt.getopt(argv,"hr:b:p:",["region=", "binLength=", "regionParts="])
+        opts, args = getopt.getopt(argv, "hr:b:p:m:", ["region=", "binLength=", "regionParts=", "annotationMode="])
     except getopt.GetoptError:
         print(usage, file=sys.stderr)
         sys.exit(2)
-     
+
     for opt, arg in opts:
         if opt == '-h':
             print(usage)
@@ -37,157 +45,195 @@ def getArgs(argv):
         elif opt in ("-b", "--binLength"):
             theArgs['binLength'] = int(arg)
         elif opt in ("-p", "--regionParts"):
-            theArgs['regionParts'] = arg         
+            theArgs['regionParts'] = arg
+        elif opt in ("-m", "--annotationMode"):
+            theArgs['annotationMode'] = arg
+
     return theArgs
-         
-def createCounts():
-    return {'TOTAL': +0, 'PATH': +0, 'BENIGN': +0, 'UNKNOWN': +0, 'OTHER': +0} 
+
+
+def createCounts(annotationMode):
+    if annotationMode == 'vep':
+        return {'TOTAL': +0, 'HIGH': +0, 'MODERATE': +0, 'MODIFIER': +0, 'LOW': +0, 'OTHER': +0}
+    else:
+        return {'TOTAL': +0, 'PATH': +0, 'BENIGN': +0, 'UNKNOWN': +0, 'OTHER': +0}
 
 
 def parseRegion(region):
-    chr           = region.split(':')[0];
-    
-    start          = region.split(':')[1].split("-")[0];
-    start          = int(start.replace(',', ''))
-    
-    end            = region.split(':')[1].split("-")[1];
-    end            = int(end.replace(',', ''))
-    length         = end - start
-    
+    chr = region.split(':')[0]
+
+    start = region.split(':')[1].split("-")[0]
+    start = int(start.replace(',', ''))
+
+    end = region.split(':')[1].split("-")[1]
+    end = int(end.replace(',', ''))
+    length = end - start
+
     return {'chr': chr, 'start': start, 'end': end, 'length': length}
 
-    
-def parseRegionIntoBins(region, binLength):   
+
+def parseRegionIntoBins(region, binLength, annotationMode):
     regionObject = parseRegion(region)
-    
-    if (binLength > 0):
-        binCount   = math.ceil(regionObject['length'] / binLength)
+
+    if binLength > 0:
+        binCount = math.ceil(regionObject['length'] / binLength)
     else:
-        binCount   = 1
-        binLength  = regionObject['length'] + 1
-        
+        binCount = 1
+        binLength = regionObject['length'] + 1
+
     bins = []
     for i in range(0, binCount):
         binStart = regionObject['start'] + (i * binLength)
-        binEnd   = min(binStart + binLength - 1, regionObject['end'])
-        bins.append({'index': i, 'start': binStart, 'end': binEnd, 'counts': createCounts()})
-        
-    
-    return { 'chr':       regionObject['chr'], 
-             'start':     regionObject['start'], 
-             'end':       regionObject['end'], 
-             'length':    regionObject['length'], 
-             'counts':    createCounts(), 
-             'binCount':  binCount, 
-             'binLength': binLength, 
-             'bins':      bins }
+        binEnd = min(binStart + binLength - 1, regionObject['end'])
+        bins.append({'index': i, 'start': binStart, 'end': binEnd, 'counts': createCounts(annotationMode)})
 
-def parseRegionPartsIntoBins(region, regionParts) :
+    return {'chr': regionObject['chr'],
+            'start': regionObject['start'],
+            'end': regionObject['end'],
+            'length': regionObject['length'],
+            'counts': createCounts(annotationMode),
+            'binCount': binCount,
+            'binLength': binLength,
+            'bins': bins}
+
+
+def parseRegionPartsIntoBins(region, regionParts, annotationMode):
     regionObject = parseRegion(region)
 
     bins = []
-    i = 0;
+    i = 0
     for regionPart in regionParts.split(","):
         binStart = int(regionPart.split("-")[0])
-        binEnd   = int(regionPart.split("-")[1])
-        bins.append({'index': i, 'start': binStart, 'end': binEnd, 'counts': createCounts()})
+        binEnd = int(regionPart.split("-")[1])
+        bins.append({'index': i, 'start': binStart, 'end': binEnd, 'counts': createCounts(annotationMode)})
         i += 1
-    
-    return { 'chr':       regionObject['chr'], 
-             'start':     regionObject['start'], 
-             'end':       regionObject['end'], 
-             'length':    regionObject['length'], 
-             'counts':    createCounts(), 
-             'binCount':  i, 
-             'binLength': int(-1), 
-             'bins':      bins }
-    
-    
+
+    return {'chr': regionObject['chr'],
+            'start': regionObject['start'],
+            'end': regionObject['end'],
+            'length': regionObject['length'],
+            'counts': createCounts(annotationMode),
+            'binCount': i,
+            'binLength': int(-1),
+            'bins': bins}
+
+
 def incrementBinIndex(record, regionBins, index):
     foundBin = False
     done = False
     while done == False:
         # record start is within bin region
         if record.start >= regionBins['bins'][index]['start'] and record.start <= regionBins['bins'][index]['end']:
-            done = True  
+            done = True
             foundBin = True
         # record start is after current bin region and we are not out of bins
         elif record.start > regionBins['bins'][index]['start'] and index < regionBins['binCount'] - 1:
-            index += 1 
-        # record start is not within any of the bin regions
+            index += 1
+            # record start is not within any of the bin regions
         else:
-            done = True            
+            done = True
 
-    binInfo = {'foundBin': foundBin, 'index': index}  
+    binInfo = {'foundBin': foundBin, 'index': index}
     return binInfo
-    
-def summarizeVariants(regionBins):
-    binIndex = 0
 
-    #vcf_reader = vcf.Reader(filename='/Users/tonyd/Downloads/vcf/clinvar.vcf.GRCh37.vcf.gz')
-    # read vcf records for a region
-    #for record in vcf_reader.fetch(regionBins['chr'], regionBins['start'], regionBins['end'] ): 
+
+# Mode determines if we're categorizing by clinvar annotations or VEP annotations
+def summarizeVariants(regionBins, annotationMode):
+    binIndex = 0
 
     # read vcf records for standard input
     vcf_reader = vcf.Reader(sys.stdin)
-    for record in vcf_reader:    
-        
+    for record in vcf_reader:
+
         binInfo = incrementBinIndex(record, regionBins, binIndex)
-        
-        if (binInfo['foundBin']):
+
+        if binInfo['foundBin']:
             binIndex = binInfo['index']
-            counts = regionBins['bins'][binIndex]['counts'];
-        
-            counts['TOTAL'] += 1;
+            counts = regionBins['bins'][binIndex]['counts']
+
+            counts['TOTAL'] += 1
             regionBins['counts']['TOTAL'] += 1
             designation = 'OTHER'
-            for cs in record.INFO['CLNSIG']:
-                for clinSig in cs.split('|'):
+
+            if annotationMode == 'clinvar':
+                for clinSig in record.INFO['CLNSIG']:
+                    # for clinSig in cs.split('|'):
                     if clinSig in CLINVAR_PATH:
                         designation = 'PATH'
-                    elif (clinSig in CLINVAR_BENIGN):
+                    elif clinSig in CLINVAR_BENIGN:
                         designation = 'BENIGN'
-                    elif (clinSig in CLINVAR_OTHER):
+                    elif clinSig in CLINVAR_OTHER:
                         designation = 'OTHER'
-                    elif (clinSig in CLINVAR_UNKNOWN):
-                        designation = 'UNKNOWN'             
+                    elif clinSig in CLINVAR_UNKNOWN:
+                        designation = 'UNKNOWN'
                     else:
                         print("warning: unknown clinsign encountered", clinSig, file=sys.stderr)
-            
+            elif annotationMode == 'vep':
+                for cs in record.INFO['CSQ']:
+                    csqArr = cs.split('|')
+                    foundField = False
+                    for csqField in csqArr:
+                        if csqField in IMPACT_FIELDS:
+                            designation = csqField
+                            foundField = True
+                            break
+                    if not foundField:
+                        print('knownvariants.sh ERROR: Problem in accessing impact keyword in CSQ INFO field', file=sys.stderr)
+
             counts[designation] += 1
             regionBins['counts'][designation] += 1
-        
+
     return regionBins
 
-def printResults(regionBins):
-    cols = ['start', 'end', 'benign', 'unknown', 'other', 'path', 'total']
-    print(*cols, sep='\t');
+
+def printResults(regionBins, annotationMode):
+    cols = []
+    if annotationMode == 'vep':
+        cols = ['start', 'end', 'high', 'moderate', 'modifier', 'low', 'other', 'total']
+    else:
+        cols = ['start', 'end', 'benign', 'unknown', 'other', 'path', 'total']
+    print(*cols, sep='\t')
     for bin in regionBins['bins']:
-        fields = [
-          bin['start'], 
-          bin['end'], 
-          bin['counts']['BENIGN'], 
-          bin['counts']['UNKNOWN'], 
-          bin['counts']['OTHER'], 
-          bin['counts']['PATH'],
-          bin['counts']['TOTAL']
-        ];
+        fields = []
+        if annotationMode == 'vep':
+            fields = [
+                bin['start'],
+                bin['end'],
+                bin['counts']['HIGH'],
+                bin['counts']['MODERATE'],
+                bin['counts']['MODIFIER'],
+                bin['counts']['LOW'],
+                bin['counts']['OTHER'],
+                bin['counts']['TOTAL']
+            ]
+        else:
+            fields = [
+                bin['start'],
+                bin['end'],
+                bin['counts']['BENIGN'],
+                bin['counts']['UNKNOWN'],
+                bin['counts']['OTHER'],
+                bin['counts']['PATH'],
+                bin['counts']['TOTAL']
+            ]
         print(*fields, sep='\t')
-    
-    
+
+
 # Get the command line arguments
 if __name__ == "__main__":
     theArgs = getArgs(sys.argv[1:])
 
-    if (theArgs['regionParts'] is None):
-        regionBins = parseRegionIntoBins(theArgs['region'], theArgs['binLength']);
+    if 'annotationMode' in theArgs and theArgs['annotationMode'] == 'vep':
+        annotationMode = 'vep'
     else:
-        regionBins = parseRegionPartsIntoBins(theArgs['region'], theArgs['regionParts'])
-   
-    binIndex = +0                            
-    regionBins = summarizeVariants(regionBins)
-    
-    printResults(regionBins);
-   
+        annotationMode = 'clinvar'
 
+    if theArgs['regionParts'] is None:
+        regionBins = parseRegionIntoBins(theArgs['region'], theArgs['binLength'], annotationMode)
+    else:
+        regionBins = parseRegionPartsIntoBins(theArgs['region'], theArgs['regionParts'], annotationMode)
 
+    binIndex = +0
+    regionBins = summarizeVariants(regionBins, annotationMode)
+
+    printResults(regionBins, annotationMode)
