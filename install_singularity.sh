f5288db99e27df263778566cda3b502bfa5e9f06
Anders Pitman <tapitman11@gmail.com>
2019-11-18 15:42:10 -0700

    Add script for installing singularity

---
+++ install_singularity.sh
@@ -0,0 +1,11 @@
+# taken from https://sylabs.io/guides/3.4/user-guide/quick_start.html#download-singularity-from-a-release
+
+export VERSION=3.4.2
+
+wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz
+tar -xzf singularity-${VERSION}.tar.gz
+cd singularity
+
+./mconfig
+make -C builddir
+sudo make -C builddir install
