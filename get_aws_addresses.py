9e19f17df222341332096ae20cec751668d0b072
Anders Pitman <tapitman11@gmail.com>
2019-11-25 14:43:09 -0700

    Add separate stage and prod deployment options

--- get_aws_addresses.py
+++ get_aws_addresses.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 import boto3
 from pprint import pprint
+import sys
+
+target = sys.argv[1]
+
+target_name = 'gru-backend-worker-0.6.0'
+if target == 'prod':
+    target_name = 'gru-backend-worker-0.5.0'
 
 client = boto3.client('ec2')
 response = client.describe_instances()
 for reservation in response['Reservations']:
     for instance in reservation['Instances']:
-        for tag in instance['Tags']:
-            if instance['State']['Name'] == 'running' and tag['Key'] == 'Name' and tag['Value'] == 'gru-backend-worker-0.5.0':
-                print(instance['PublicDnsName'])
+        if 'Tags' in instance:
+            for tag in instance['Tags']:
+                if instance['State']['Name'] == 'running' and tag['Key'] == 'Name' and tag['Value'] == target_name:
+                    print(instance['PublicDnsName'])
