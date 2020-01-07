2ddbcfab31089a16a5521e503f51509541f77cee
Ubuntu <ubuntu@ip-172-30-0-135.ec2.internal>
2020-01-07 23:35:33 +0000

    Working without quality phrase

--- scripts/getSomaticVariants.sh
+++ scripts/getSomaticVariants.sh
@@ -10,44 +10,38 @@ tumorAfCutoff=$7
 normalSampleIdx=$8
 totalSampleNum=$9
 
-# have to translate to 1 based indexing
-$normalSampleIdx = $normalSampleIdx + 1
-
 qualPhrase="QUAL>${qualCutoff}"
+
+# TODO: this needs to be AO cumulative of all genotypes
+# if we have an alt in sample we're looking at, must pass quality threshold (
 depthPhrase="AN>${totalReadCutoff}"
+
 normalCountPhrase="AC[${normalSampleIdx}]<=${normalCountCutoff}"
-normalAfPhrase="AF[${normalSampleIdx}]<=${normalAfCutoff}||(AC[${normalSampleIdx}]/AN)<=${normalAfCutoff}"
+normalAfPhrase="(AF[${normalSampleIdx}]<=${normalAfCutoff}||(AC[${normalSampleIdx}]/AN)<=${normalAfCutoff})"
 
 tumorCountPhrase="("
 tumorAfPhrase="("
-for i in $totalSampleNum; do
-	if ($i != $normalSampleIdx)
-		j=i+1
-		$tumorCountPhrase="${tumorCountPhrase}AC[${j}]>=${tumorCountCutoff}"
-		$tumorAfPhrase="${tumorAfPhrase}((AF[${j}]>=${tumorAfCutoff})||((AC[${j}]/AN)<=${tumorAfCutoff}))"
-		if ($i < $totalSampleNum-1)
-			$tumorCountPhrase="${tumorCountPhrase}||"
-			$tumorAfPhrase="$tumorAfPhrase||"
+for ((i=0; i<totalSampleNum; i++)); do
+	if ((i != normalSampleIdx)); then
+		tumorCountPhrase="${tumorCountPhrase}AC[${i}]>=${tumorCountCutoff}"
+		tumorAfPhrase="${tumorAfPhrase}((AF[${i}]>=${tumorAfCutoff})||((AC[${i}]/AN)<=${tumorAfCutoff}))"
+		if ((i < totalSampleNum-1)); then
+			tumorCountPhrase="${tumorCountPhrase}||"
+			tumorAfPhrase="$tumorAfPhrase||"
 		fi
 	fi
 done
-$tumorCountPhrase="${tumorCountPhrase})"
-$tumorAfPhrase="${tumorAfPhrase})"
-echo $tumorCountPhrase
-echo $tumorAfPhrase
+tumorCountPhrase="${tumorCountPhrase})"
+tumorAfPhrase="${tumorAfPhrase})"
 
-queryPhrase="${qualPhrase}&&${depthPhrase}&&${normalCountPhrase}&&${normalAfPhrase}&&${tumorCountPhrase}&&${tumorAfPhrase}"
+queryPhrase="${qualPhrase}&&${normalCountPhrase}&&${normalAfPhrase}&&${tumorCountPhrase}&&${tumorAfPhrase}"
 echo $queryPhrase
 
 runDir=$PWD
 tempDir=$(mktemp -d)
 cd $tempDir
 
-#bcftools query -f '%CHROM %POS %REF %ALT %QUAL %INFO %FILTER' | awk '{ if ($5 >= int($qualCutoff) print $1 $2 $3 $4 $5 $6 $7 }'
-
-bcftools query -f '%LINE\n' -i $queryPhrase $vcfUrl
-
-#awk '{ if (int($6) > int($qualCutoff)) print $6 }'
+bcftools query -f '%LINE\n' -i $queryPhrase $vcfUrl | head -10
 
 #echo $tempDir
 rm -rf $tempDir
