3372445e4f95672e7b5347947a4503242e04fcb1
stefinfection <sjeangeo@gmail.com>
2020-03-25 19:38:04 +0000

    Added curl checks to bam/bai

--- scripts/checkBamBai.sh
+++ scripts/checkBamBai.sh
@@ -4,4 +4,11 @@ url=$1
 index_url=$2
 samtools_region=$3
 
+url_valid=$(curl -s $url | head -n 1 | tr '\0' '\n')
+index_valid=$(curl -s $index_url | head -n 1 | tr '\0' '\n')
+if [ -z "$url_valid" ] || [ -z "$index_valid" ]; 
+then
+	exit 1;
+fi
+
 samtools_od view -b $url $samtools_region $index_url | head -n 1
