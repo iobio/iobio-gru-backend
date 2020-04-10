c06c0a5e58cabcf1ace63760e4f75b38701a03cb
Anders Pitman <tapitman11@gmail.com>
2020-04-10 14:21:48 -0600

    Update AWS deployment to 0.7.0

--- get_aws_addresses.py
+++ get_aws_addresses.py
@@ -5,7 +5,7 @@ import sys
 
 target = sys.argv[1]
 
-target_name = 'gru-backend-worker-0.6.0'
+target_name = 'gru-backend-worker-0.7.0'
 if target == 'prod':
     target_name = 'gru-backend-worker-0.6.0'
 
