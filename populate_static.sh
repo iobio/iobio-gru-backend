cd9249b25ca388214b8b5152989ac9426e5360ad
Anders Pitman <tapitman11@gmail.com>
2020-04-10 14:39:45 -0600

    Add rolling clinvar files

--- populate_static.sh
+++ populate_static.sh
@@ -13,3 +13,10 @@ wget -P static/GRCh38/2018 ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/arc
 mkdir -p static/GRCh38/2019
 wget -P static/GRCh38/2019 ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/archive_2.0/2019/clinvar_20191021.vcf.gz
 wget -P static/GRCh38/2019 ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/archive_2.0/2019/clinvar_20191021.vcf.gz.tbi
+
+mkdir -p static/clinvar/GRCh37
+wget -P static/clinvar/GRCh37 https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz
+wget -P static/clinvar/GRCh37 https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz.tbi
+mkdir -p static/clinvar/GRCh38
+wget -P static/clinvar/GRCh38 https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz
+wget -P static/clinvar/GRCh38 https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz.tbi
