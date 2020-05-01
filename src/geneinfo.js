6ea97f5894383e4b83235eb42a13d7cf4be30bbc
stefinfection <sjeangeo@gmail.com>
2020-05-01 21:42:59 +0000

    Add new endpoint /api/genes to support GET request for multiple genes at once

--- src/geneinfo.js
+++ src/geneinfo.js
@@ -5,6 +5,22 @@ var async = require('async');
 const Router = require('koa-router');
 const router = new Router();
 
+
+getGenesInClause = function(genes) {
+  sqlString = " in (";
+  let firstTime = true;
+  genes.forEach(function(gene) {
+    if (firstTime) {
+      firstTime = false;
+    } else {
+      sqlString += ",";
+    }
+    sqlString += "\""+gene+"\" ";
+  }) 
+  sqlString += ")";
+  return sqlString
+}
+
 router.get('/api/gene/:gene', async (ctx) => {  
   var source = ctx.query.source;
   var species = ctx.query.species;
@@ -86,6 +102,88 @@ router.get('/api/gene/:gene', async (ctx) => {
   });
 });
 
+
+router.get('/api/genes/', async (ctx) => {  
+
+  var genesString = ctx.query.genes;
+  var genes = genesString.split(",")
+
+  var source = ctx.query.source;
+  var species = ctx.query.species;
+  var build = ctx.query.build;
+  if (source == null || source == '') {
+    source = 'gencode';
+  } 
+  var geneSqlString = "SELECT distinct * from genes where gene_name ";
+  geneSqlString    += getGenesInClause(genes);
+  geneSqlString    += " AND source = \""+source+"\"";
+  if (species != null && species != "") {
+    geneSqlString  += " AND species = \""+species+"\"";
+  }
+  if (build != null && build != "") {
+    geneSqlString  += " AND build = \""+build+"\"";
+  }
+
+  return new Promise((resolve, reject) => {
+    db.all(geneSqlString,function(err,rows){ 
+      var gene_data = {};
+
+      if (rows != null && rows.length > 0) {
+        for (var i = 0; i < rows.length; i++) {
+          let gene_record = rows[i]
+          gene_record.transcripts = []
+          gene_data[gene_record.gene_name] = gene_record;    
+        }
+      } 
+          
+      var source = ctx.query.source; 
+      if (source == null || source == '') {
+        source = 'gencode';
+      } 
+      var sqlString = "";
+      if (source == 'gencode') {
+        sqlString =  "SELECT t.*, x.refseq_id as 'xref' from transcripts t ";
+        sqlString += "LEFT OUTER JOIN xref_transcript x on x.gencode_id = t.transcript_id ";
+      } else if (source == 'refseq') {
+        sqlString =  "SELECT t.*, x.gencode_id as 'xref' from transcripts t ";
+        sqlString += "LEFT OUTER JOIN xref_transcript x on x.refseq_id = t.transcript_id ";
+      }
+      sqlString     += "WHERE t.gene_name ";
+      sqlString     += getGenesInClause(genes);
+      sqlString +=    "AND t.source = \""+source+"\"";
+      if (species != null && species != "") {
+        sqlString  += " AND t.species = \""+species+"\"";
+      }
+      if (build != null && build != "") {
+        sqlString  += " AND t.build = \""+build+"\"";
+      }        
+      db.all(sqlString,function(err,transcriptRows){    
+
+        if (err) reject(err);
+
+
+        if (transcriptRows != null && transcriptRows.length > 0) {
+          for (var i = 0; i < transcriptRows.length; i++) {
+            var transcript = transcriptRows[i]; 
+            transcript['features'] = JSON.parse(transcript['features']);
+            gene_record = gene_data[transcript.gene_name]
+            if (gene_record) {
+              gene_record["transcripts"].push(transcript);
+            } else {
+              console.log("cannot find gene for transcript " + transcript.transcript_id + " " + transcript.gene_name)
+            }  
+          }
+        }
+
+        ctx.set('Content-Type', 'application/json');
+        ctx.set('Charset', 'utf-8')
+        ctx.body = ctx.query.callback + '(' + JSON.stringify([gene_data]) +');';
+        resolve();
+      });
+    });
+  });
+});
+
 router.get('/api/region/:region', async (ctx) => {  
   var chr = ctx.params.region.split(':')[0].toLowerCase();
   var start = ctx.params.region.split(':')[1].split('-')[0];
@@ -96,7 +194,7 @@ router.get('/api/region/:region', async (ctx) => {
   if (source == null || source == '') {
     source = 'gencode';
   } 
-  var sqlString = "SELECT * from genes where chr = '" + chr 
+  var sqlString = "SELECT distinct * from genes where chr = '" + chr 
     + "' and  (start between " + start + " and " + end 
     + "        or end between " + start + " and " + end + ")";
   if (species != null && species != "") {
@@ -105,10 +203,12 @@ router.get('/api/region/:region', async (ctx) => {
   if (build != null && build != "") {
     sqlString  += " AND build = \""+build+"\"";
   }  
+  if (source != null && source != "") {
+    sqlString +=    " AND source = \""+source+"\"";         
+  }
 
   return new Promise((resolve, reject) => {
     db.all(sqlString, function(err, genes) {  
-      
       async.map(genes, 
         function(gene_data, outterDone) {                   
           var transcript_ids = JSON.parse(gene_data['transcripts']);
@@ -122,7 +222,9 @@ router.get('/api/region/:region', async (ctx) => {
                 sqlString +=    "LEFT OUTER JOIN xref_transcript x on x.refseq_id = t.transcript_id ";
               }
               sqlString +=    "WHERE t.transcript_id=\""+id+"\" "
-              sqlString +=    "AND t.source = \""+source+"\"";          
+              if (source != null && source != "") {
+		sqlString +=    " AND t.source = \""+source+"\""; 
+	      }
               if (species != null && species != "") {
                 sqlString  += " AND t.species = \""+species+"\"";
               }
@@ -161,3 +263,4 @@ router.get('/api/region/:region', async (ctx) => {
 });
 
 module.exports = router;
+
