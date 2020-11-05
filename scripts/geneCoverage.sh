166905dc0a65fd7d88a71af4fe253b7b015987eb
Anders Pitman <tapitman11@gmail.com>
2020-11-05 15:58:10 -0700

    Use mktemp for temp directories of scripts

--- scripts/geneCoverage.sh
+++ scripts/geneCoverage.sh
@@ -5,22 +5,18 @@ index_url=$2
 samtools_region=$3
 region_file_str=$4
 
-while true; do
-    tmp_dir=/tmp/iobio_gene_coverage_$(cat /proc/sys/kernel/random/uuid)
-    if [ ! -d $tmp_dir ]; then
-        break;
-    fi
-done
-
-mkdir $tmp_dir
+runDir=$PWD
+tempDir=$(mktemp -d)
+cd $tempDir
 
 # TODO: it doesn't seem to work unless I have a file extension on the end...
-printf "$region_file_str" > $tmp_dir/region_file.txt
+printf "$region_file_str" > region_file.txt
 
-samtools_od view -b $url $samtools_region $index_url > $tmp_dir/alignment.aln
+samtools_od view -b $url $samtools_region $index_url > alignment.aln
 
-samtools index $tmp_dir/alignment.aln
+samtools index alignment.aln
 
-geneCoverage -b $tmp_dir/alignment.aln -r $tmp_dir/region_file.txt
+geneCoverage -b alignment.aln -r region_file.txt
 
-rm -rf $tmp_dir
+cd $runDir
+rm -rf $tempDir
