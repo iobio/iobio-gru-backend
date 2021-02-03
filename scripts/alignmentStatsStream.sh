d26611bf21bd259f33fcb80ec8b85b501d242c39
Anders Pitman <tapitman11@gmail.com>
2021-02-03 14:39:03 -0700

    Run alignmentStatsStream in a temp directory
    Before it was using samtools_od, which had its own temp directory.

--- scripts/alignmentStatsStream.sh
+++ scripts/alignmentStatsStream.sh
@@ -6,6 +6,9 @@ samtoolsRegion=$3
 bamstatsRegions=$4
 dataDir=$5
 
+tempDir=$(mktemp -d)
+cd $tempDir
+
 dataOpts=$url
 if [ -n "${indexUrl}" ]; then
     dataOpts="-X $url $indexUrl"
@@ -14,3 +17,5 @@ fi
 export REF_CACHE=$dataDir/md5_reference_cache/%2s/%2s/%s
 
 samtools-1.11 view -b $dataOpts $samtoolsRegion | bamstatsAlive -u 500 -k 1 -r $bamstatsRegions
+
+rm -rf $tempDir
