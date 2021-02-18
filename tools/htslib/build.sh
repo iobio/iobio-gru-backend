eacce2cc1cc61f288eddff929ad36b9d145b4fe7
Anders Pitman <tapitman11@gmail.com>
2021-02-17 17:51:37 -0700

    Add bcftools to htslib Dockerfile

--- tools/htslib/build.sh
+++ tools/htslib/build.sh
@@ -2,7 +2,7 @@
 
 HTSLIB_VERSION=${1}
 SAMTOOLS_VERSION=${HTSLIB_VERSION}
-
+BCFTOOLS_VERSION=${HTSLIB_VERSION}
 
 curl -LO https://github.com/samtools/samtools/releases/download/${SAMTOOLS_VERSION}/samtools-${SAMTOOLS_VERSION}.tar.bz2
 tar xvf samtools-${SAMTOOLS_VERSION}.tar.bz2
@@ -20,3 +20,11 @@ cd ..
 ./configure --without-curses
 make -j4
 strip samtools
+
+cd /
+curl -LO https://github.com/samtools/bcftools/releases/download/${BCFTOOLS_VERSION}/bcftools-${BCFTOOLS_VERSION}.tar.bz2
+tar xvf bcftools-${BCFTOOLS_VERSION}.tar.bz2
+cd bcftools-${BCFTOOLS_VERSION}
+./configure --without-curses
+make -j4
+strip bcftools
