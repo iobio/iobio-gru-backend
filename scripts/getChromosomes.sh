d18bf2c58fa22195cd9954e1fc577cf6941ae9a8
Anders Pitman <tapitman11@gmail.com>
2021-03-11 17:14:48 -0700

    Remove remaining tabix_od uses

--- scripts/getChromosomes.sh
+++ scripts/getChromosomes.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-tabix_od -l $1 $2
+vcfUrl=$1
+tbiUrl=$2
+
+tempDir=$(mktemp -d)
+cd $tempDir
+
+tabixVcfArg=$vcfUrl
+if [ -n "${tbiUrl}" ]; then
+    tabixVcfArg="$vcfUrl##idx##$tbiUrl"
+fi
+
+tabix -l $tabixVcfArg
+
+rm -rf $tempDir
--- scripts/vcfStatsStream.sh
+++ scripts/vcfStatsStream.sh
@@ -6,6 +6,11 @@ regionsStr=$3
 contigStr=$4
 sampleNamesStr=$5
 
+tabixVcfArg=$url
+if [ -n "${indexUrl}" ]; then
+    tabixVcfArg="$url##idx##$indexUrl"
+fi
+
 tempDir=$(mktemp -d)
 cd $tempDir
 
@@ -13,12 +18,12 @@ echo -e "$contigStr" > contigs.txt
 
 if [ "$sampleNamesStr" ]; then
         echo -e "$sampleNamesStr" > sample_names.txt
-        tabix_od -h $url $regionsStr $indexUrl | \
+        tabix -h $tabixVcfArg $regionsStr | \
                 bcftools annotate -h contigs.txt - | \
                 vt subset -s sample_names.txt - | \
                 vcfStatsAlive -u 1000 -Q 1000
 else
-        tabix_od -h $url $regionsStr $indexUrl | \
+        tabix -h $tabixVcfArg $regionsStr | \
                 bcftools annotate -h contigs.txt - | \
                 vcfStatsAlive -u 1000 -Q 1000
 fi
