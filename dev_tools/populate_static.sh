a8a0d32041a072c7ba8f54daa5df1e865345d479
Anders Pitman <tapitman11@gmail.com>
2021-03-24 15:26:23 -0600

    Change populate_static.sh to use curl

--- dev_tools/populate_static.sh
+++ dev_tools/populate_static.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir -p static/clinvar/GRCh37
-wget -P static/clinvar/GRCh37 https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz
-wget -P static/clinvar/GRCh37 https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz.tbi
+curl -o static/clinvar/GRCh37/clinvar.vcf.gz https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz
+curl -o static/clinvar/GRCh37/clinvar.vcf.gz.tbi https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar.vcf.gz.tbi
 mkdir -p static/clinvar/GRCh38
-wget -P static/clinvar/GRCh38 https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz
-wget -P static/clinvar/GRCh38 https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz.tbi
+curl -o static/clinvar/GRCh38/clinvar.vcf.gz https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz
+curl -o static/clinvar/GRCh38/clinvar.vcf.gz.tbi https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz.tbi
