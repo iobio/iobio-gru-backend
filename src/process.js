70cb8611d209a071da8948da63f3b5b0f939910b
Anders Pitman <tapitman11@gmail.com>
2022-03-15 15:52:09 -0600

    Remove old commented code

--- src/process.js
+++ src/process.js
@@ -1,11 +1,6 @@
 const spawn = require('child_process').spawn;
 
 
-// The purpose of this wrapper is to detect error conditions on the child
-// process, before yielding the process to be piped. The basic idea is to
-// listen to stdout and stderr, and watch for the exit event. If stdout
-// receives data before stderr and/or exit, then the process is provided to
-// the caller, otherwise the promise is rejected.
 async function run(path, args, options) {
 
   const proc = spawn(path, args);
@@ -18,24 +13,13 @@ async function run(path, args, options) {
 
   return new Promise((resolve, reject) => {
 
-    //proc.stdout.setEncoding('utf8');
     proc.stderr.setEncoding('utf8');
 
-    //const BUF_SIZE = 1024;
-    //let buf = "";
-
     function onStdout(data) {
-
-      //buf += data;
-
-      //if (buf.length > BUF_SIZE) {
-        proc.stdout.removeListener('data', onStdout);
-        proc.stdout.pause();
-        proc.stdout.unshift(data);
-        //proc.stdout.unshift(buf);
-        //buf = "";
-        resolve(proc);
-      //}
+      proc.stdout.removeListener('data', onStdout);
+      proc.stdout.pause();
+      proc.stdout.unshift(data);
+      resolve(proc);
     }
     proc.stdout.on('data', onStdout);
 
@@ -56,13 +40,6 @@ async function run(path, args, options) {
         settled = true;
 
         if (e === 0) {
-          // Just in case the overall length is less than BUF_SIZE, make sure
-          // it gets passed on.
-          //if (buf.length > 0) {
-          //  proc.stdout.pause();
-          //  console.log("unshift");
-          //  proc.stdout.unshift(buf);
-          //}
           resolve(proc);
         }
         else {
