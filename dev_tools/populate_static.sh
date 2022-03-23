0a89ffaeffef0943098716975fa2d8feb80fb7ab
Anders Pitman <tapitman11@gmail.com>
2022-03-23 15:32:30 -0600

    Update ClinVar to 20220320

--- dev_tools/populate_static.sh
+++ dev_tools/populate_static.sh
@@ -3,8 +3,8 @@
 set -e
 
 mkdir -p static/clinvar/GRCh37
-curl --fail -o static/clinvar/GRCh37/clinvar.vcf.gz https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/weekly/clinvar_20211204.vcf.gz
-curl --fail -o static/clinvar/GRCh37/clinvar.vcf.gz.tbi https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/weekly/clinvar_20211204.vcf.gz.tbi
+curl --fail -o static/clinvar/GRCh37/clinvar.vcf.gz https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/weekly/clinvar_20220320.vcf.gz
+curl --fail -o static/clinvar/GRCh37/clinvar.vcf.gz.tbi https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/weekly/clinvar_20220320.vcf.gz.tbi
 mkdir -p static/clinvar/GRCh38
-curl --fail -o static/clinvar/GRCh38/clinvar.vcf.gz https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/weekly/clinvar_20211204.vcf.gz
-curl --fail -o static/clinvar/GRCh38/clinvar.vcf.gz.tbi https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/weekly/clinvar_20211204.vcf.gz.tbi
+curl --fail -o static/clinvar/GRCh38/clinvar.vcf.gz https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/weekly/clinvar_20220320.vcf.gz
+curl --fail -o static/clinvar/GRCh38/clinvar.vcf.gz.tbi https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/weekly/clinvar_20220320.vcf.gz.tbi
