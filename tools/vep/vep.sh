5f75eac9b3c9278734584af68ed38e2307ddec91
Anders Pitman <tapitman11@gmail.com>
2023-03-01 23:07:39 -0700

    Remove some unused tools
    The samtools and bcftools are now provided by the htslib tool.
    
    VEP is provided implicitly because we derive our dockerfile from
    theirs.

--- tools/vep/vep.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Incantation taken from https://stackoverflow.com/a/246128/943814
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-singularity exec $SCRIPT_DIR/vep.sif vep \
-    -i STDIN --format vcf --cache --dir_cache $SCRIPT_DIR/../vep-cache \
-    --offline --vcf -o STDOUT --no_stats --no_escape --sift b --polyphen b \
-    --regulatory --fork 4 $@
