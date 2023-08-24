d4d024e2dc6d148c1094bcf9a2a679f16c7f21ce
Tony Di Sera <tonydisera@users.noreply.github.com>
2023-08-24 11:50:17 -0600

    Expand gene2pheno.js
    Added a new endpoint /associations that will return both phenotypes as well as disorders associated with a gene

--- src/gene2pheno.js
+++ src/gene2pheno.js
@@ -10,6 +10,26 @@ function getDb() {
   return _db;
 }
 
+function promiseGetGeneDisorders(db, gene_name) {
+  var sqlString = "SELECT * from gene_to_disorder where gene_symbol=\""+gene_name+"\" ";
+  return new Promise((resolve, reject) => {
+    db.all(sqlString,function(err,rows){ 
+
+      if (err) reject(err);
+
+      var disorder_data = {};
+      var disorders = [];
+      if (rows != null && rows.length > 0) {
+        for (var i = 0; i < rows.length; i++) {
+          disorder_data = rows[i];           
+          disorders.push(disorder_data);
+        }
+      } 
+      resolve(disorders)
+    })
+  })
+}
+
 const router = new Router();
 
 router.get('/api/gene/:gene', async (ctx) => {
@@ -67,6 +87,51 @@ router.get('/:gene', async (ctx) => {
       ctx.body = JSON.stringify(phenotypes);
 
       resolve();
+    });
+  });
+});
+
+// v3 In addition to returning associated phenotypes, this endpoint also returns 
+// associated disorders for a gene.
+router.get('/associations/:gene', async (ctx) => {
+  var sqlString = "SELECT * from gene_to_phenotype where gene_symbol=\""+ctx.params.gene+"\" ";
+
+  const db = getDb();
+
+  return new Promise((resolve, reject) => {
+    db.all(sqlString,function(err,rows){ 
+
+      if (err) reject(err);
+
+      var phenotype_data = {};
+      var phenotypes = [];
+      if (rows != null && rows.length > 0) {
+        for (var i = 0; i < rows.length; i++) {
+          phenotype_data = rows[i];           
+          phenotypes.push(phenotype_data);
+        }
+      } 
+
+      promiseGetGeneDisorders(db, ctx.params.gene)
+      .then(function(disorders) {
+
+        let associations = {'gene': ctx.params.gene, 
+                            'phenotypes': phenotypes, 
+                            'disorders': disorders}
+      
+        ctx.set('Content-Type', 'application/json');
+        ctx.set('Charset', 'utf-8');
+        ctx.set('Cache-Control', 'public,max-age=84600')
+        ctx.body = JSON.stringify(associations);
+
+        resolve();
+
+      })
+      .catch(function(error) {
+        reject(error)
+      })
+
+
     });
   });
 });
