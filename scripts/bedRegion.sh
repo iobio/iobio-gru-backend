845abe4b9d01774e7565a4f6a86c5a20f2682c85
Tony Di Sera <tonydisera@users.noreply.github.com>
2023-08-31 13:25:58 -0600

    Update bedRegion.sh
    creating of temp dir handled by iobio gru backend. No need to accomodate this in bash script.

--- scripts/bedRegion.sh
+++ scripts/bedRegion.sh
@@ -10,12 +10,4 @@ if [ -n "${indexUrl}" ]; then
     tabixBedArg="$url##idx##$indexUrl"
 fi
 
-runDir=$PWD
-tempDir=$(mktemp -d)
-cd $tempDir
-
-
 tabix $tabixBedArg $regionsStr
-
-cd $runDir
-rm -rf $tempDir
