3acf4c3ca12f3b76c18f360325d671fd389babfb
Anders Pitman <&ers@apitman.com>
2024-03-26 13:51:22 -0600

    Default to using git describe for version

--- dev_tools/build_docker_image.sh
+++ dev_tools/build_docker_image.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-gru_version=${1}
+gru_version_arg=${1}
+
+if [ -z ${gru_version_arg} ]
+then
+    gru_version=$(git describe --tags)
+else
+    gru_version=$gru_version_arg
+fi
+
+echo "Building version $gru_version"
 
 docker build -t iobio/iobio-gru-backend:${gru_version} -f docker/prod/Dockerfile .
