7d8dda47e82ec47a3bae198c0620815175e41837
Anders Pitman <&ers@apitman.com>
2024-04-01 17:00:08 -0600

    Move clinvar download into separate file

---
+++ dev_tools/download_clinvar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+clinvar_version=20240331
+
+mkdir -p static/clinvar/GRCh37
+curl --fail -o static/clinvar/GRCh37/clinvar.vcf.gz https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/weekly/clinvar_${clinvar_version}.vcf.gz
+curl --fail -o static/clinvar/GRCh37/clinvar.vcf.gz.tbi https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/weekly/clinvar_${clinvar_version}.vcf.gz.tbi
+mkdir -p static/clinvar/GRCh38
+curl --fail -o static/clinvar/GRCh38/clinvar.vcf.gz https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/weekly/clinvar_${clinvar_version}.vcf.gz
+curl --fail -o static/clinvar/GRCh38/clinvar.vcf.gz.tbi https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/weekly/clinvar_${clinvar_version}.vcf.gz.tbi
