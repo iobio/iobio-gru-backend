a1e33ff8c1d111df49025417e074f74d5179f4ec
Anders Pitman <&ers@apitman.com>
2026-08-03 21:32:51 +0000

    Add Podman image build script

---
+++ dev_tools/build_podman_image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+gru_version_arg=${1}
+
+if [ -z ${gru_version_arg} ]
+then
+    gru_version=$(git describe --tags)
+else
+    gru_version=$gru_version_arg
+fi
+
+echo "${gru_version}" > VERSION
+echo "Building version $gru_version"
+
+podman build -t docker.io/iobio/iobio-gru-backend:${gru_version} -f docker/prod/Dockerfile .
